842ee0be0696b79bf9d383e516de6af01f2f8265
tsh <tsh989@hotmail.com>
2020-07-10 19:28:52 +0800

    tianshuo0710

---
+++ day0710tianshuo/src/main/java/com/practise/controller/SignController.java
@@ -0,0 +1,56 @@
+package com.practise.controller;
+
+import com.practise.util.JDBCUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @Auther: tsh
+ * @program: tianshuo
+ * @Description:
+ * @Date: 2020/7/10 15:18
+ **/
+@WebServlet("/signController")
+public class SignController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        int flag=0;
+        Connection conn = JDBCUtils.getConn();
+        String sql = "select * from user";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            wh:while (rs.next()) {
+                if (rs.getString(2).equals(username)
+                        && rs.getString(3).equals(password)) {
+                    flag=1;
+                    HttpSession session = req.getSession();
+                    session.setAttribute("username",username);
+                    resp.getWriter().println(flag);
+
+                    break wh;
+                }
+            }
+            if(flag==0){
+                resp.getWriter().println(flag);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ day0710tianshuo/src/main/java/com/practise/util/JDBCUtils.java
@@ -0,0 +1,55 @@
+package com.practise.util;
+
+import java.sql.*;
+
+/**
+ * @Auther: tsh
+ * @program: workspace
+ * @Description:
+ * @Date: 2020/6/29 12:29
+ **/
+public class JDBCUtils {
+    private static final String DRIVER="com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/day0710";
+    private static final String USER = "root";
+    private static final String PASSWORD="123456";
+
+    /**
+     * 获取连接对象的公共方法
+     * */
+    public static Connection getConn(){
+        Connection conn=null;
+        try{
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    /**
+     * 释放资源-增删改
+     */
+    public static void closeAll(Connection conn, PreparedStatement ps) {
+        try {
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 释放资源-包含查询
+     */
+    public static void closeAll(Connection conn, PreparedStatement ps,ResultSet rs) {
+        try {
+            rs.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
